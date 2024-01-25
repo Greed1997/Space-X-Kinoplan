d56c496d7336e4db836290612ce12572d487126d
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2024-01-25 14:16:31 +0300

    Initial Commit

---
+++ SpaceX_Kinoplan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX_Kinoplan
+//
+//  Created by Александр on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
